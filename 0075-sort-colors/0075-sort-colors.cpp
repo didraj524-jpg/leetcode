1fa9f2a7065619a1efa07466e6c8ead04d943395
Dipjyoti <didraj524@gmail.com>
2026-06-23 14:49:01 +0530

    Time: 0 ms (100%), Space: 11.5 MB (97.78%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        vector<int>arr1;
-        vector<int>arr2;
-        vector<int>arr3;
 
 
-        for(int i=0;i<nums.size();i++){
-             if(nums[i]==0){
-                arr1.push_back(0);
-             }
-             if(nums[i]==1){
-                arr2.push_back(1);
-             }
-             if(nums[i]==2){
-                arr3.push_back(2);
-             }
+        int high=nums.size()-1;
+        int low=0;
+        int mid=0;
+        while (mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid],nums[high]);
+                high--;
+            }
         }
-        nums.clear();
-      for(int i=0;i<arr1.size();i++){
-        nums.push_back(0);
-      }
-         for(int i=0;i<arr2.size();i++){
-        nums.push_back(1);
-      }
-         for(int i=0;i<arr3.size();i++){
-        nums.push_back(2);
-      }
-       
-       for(int i=0;i<nums.size();i++){
-        cout<<nums[i];
-       }
         
     }
 };
