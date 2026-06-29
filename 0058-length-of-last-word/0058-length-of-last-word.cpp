8c2cf6e74f176e86f8d2bd49f242e965a8e83ff7
Dipjyoti <didraj524@gmail.com>
2026-06-30 02:03:09 +0600

    Time: 0 ms (100%), Space: 9 MB (9.08%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int n=s.length();int count=0;int k=0;
-      for(int i=n-1;i>=0;i--){
-        if(s[n-1]!=' '){
-        if (s[i]!=' '){
-            count++;
+        int n = s.length();
+        int count = 0;
+        int k = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[n - 1] != ' ') {
+                if (s[i] != ' ') {
+                    count++;
+                }
+                if (s[i] == ' ' && s[i + 1] != ' ') {
+                    return count;
+                }
+            }
+            if (s[n - 1] == ' ') {
+                if (s[i] != ' ') {
+                    count++;
+                }
+                if (s[i] == ' ' && s[i + 1] != ' ') {
+                    k++;
+                    if (k > 1) {
+                        return count;
+                    }
+                }
+            }
         }
-         if(s[i]==' '&&s[i+1]!=' '){
-            return count;
-         }
-        }
-        if(s[n-1]==' '){
-             if (s[i]!=' '){
-            count++;
-        }
-        if(s[i]==' '&&s[i+1]!=' '){
-            k++;
-            if(k>1){return count;}
-        }
-        }
-      }
 
-      return count; 
+        return count;
     }
 };
