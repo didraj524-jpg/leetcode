5b75ff8374f953eea676b738844cbcb2a23c3d35
Dipjyoti <didraj524@gmail.com>
2026-07-03 11:56:22 +0600

    Time: 3 ms (59.75%), Space: 117.8 MB (93.59%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,30 +1,55 @@
 /**
  * Definition for singly-linked list.
  * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * int val;
+ * ListNode *next;
+ * ListNode() : val(0), next(nullptr) {}
+ * ListNode(int x) : val(x), next(nullptr) {}
+ * ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        if(head==NULL||head->next==NULL)return true;
-        vector<int>arr;
-        ListNode*temp=head;
-        while(temp!=NULL){
-            arr.push_back(temp->val);
-            temp=temp->next;
-        }
-
-        int n=arr.size();
+    ListNode* reverseLL(ListNode* start) {
+        ListNode* prev = nullptr;
+        ListNode* current = start;
         
-        for(int i=0;i<n/2;i++){
-            if(arr[i]!=arr[n-i-1]){return false;}
+        while (current != nullptr) {
+            ListNode* nextptr = current->next;
+            current->next = prev;
+            prev = current;
+            current = nextptr;
+        }
+        return prev; 
+    }
+
+    bool isPalindrome(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) return true;
+
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        // 1. Find the middle configuration
+        while (fast->next != nullptr && fast->next->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
+        // 2. Reverse the second half
+        ListNode* newHead = reverseLL(slow->next);
+
+        // 3. Compare halves
+        ListNode* firstHalf = head;
+        ListNode* secondHalf = newHead;
+        
+        while (secondHalf != nullptr) {
+            if (firstHalf->val != secondHalf->val) {
+                return false; 
+            }
+            firstHalf = firstHalf->next;
+            secondHalf = secondHalf->next;
+        }
+        
         return true;
     }
 };
